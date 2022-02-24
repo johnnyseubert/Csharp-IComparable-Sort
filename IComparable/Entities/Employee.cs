2ccb62f60031e9278f817089db0abd23f24654d2
DATA53454 <data53454@datainfo.inf.br>
2022-02-24 11:49:47 -0300

    Adicionando arquivos

---
+++ IComparable/Entities/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+namespace IComparable.Entities
+{
+    public class Employee : System.IComparable
+    {
+        //properties
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        //constructor
+        public Employee(string cvsEmployee)
+        {
+            string[] vect = cvsEmployee.Split(',');
+            Name = vect[0];
+            Salary = double.Parse(vect[1], CultureInfo.InvariantCulture);
+        }
+
+        //methods
+        public override string ToString()
+        {
+            return $"{Name},{Salary.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (!(obj is Employee))
+            {
+                throw new ArgumentException("Comparing error: arguiment is not and Employee");
+            }
+            
+            Employee other = obj as Employee;
+            return Name.CompareTo(other.Name);
+        }
+    }
+}
---
+++ IComparable/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using IComparable.Entities;
+
+namespace IComparable
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string path = @"C:\temp\in.txt";
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    List<Employee> list = new List<Employee>();
+                    
+                    while (!sr.EndOfStream)
+                    {
+                        list.Add(new Employee(sr.ReadLine()));
+                    }
+                    list.Sort();
+                    foreach (var emp in list)
+                    {
+                        Console.WriteLine(emp);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
